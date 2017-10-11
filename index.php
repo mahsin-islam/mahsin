d4cb19e1cb3d49580ce0750ddd8e07931b5f3d8d
MD. Mahsin-Ul-Islam <mahsin.islam@gmail.com>
2017-10-11 21:09:04 +0600

    again change
    new code

---
+++ index.php
@@ -0,0 +1,207 @@
+
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>MD. Mahsin-Ul-Islam | Software Developer | Dhaka, Bangladesh</title>
+<meta name="description" content="I&#39;m a web &amp; software developer, primarily working with Java, JavaScript, php and Laravel, living in Dhaka, Bangladesh.">
+
+<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Work+Sans:400,500,700">
+<link rel="stylesheet" href="assets/css/main.css">
+
+<link rel="canonical" href="https://mahsin.me/">
+
+
+<link rel="icon" type="image/x-icon" href="images/favicon.ico">
+<link rel="apple-touch-icon" sizes="57x57" href="assets/images/favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="assets/images/favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="assets/images/favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="assets/images/favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="assets/images/favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="assets/images/favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="assets/images/favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="assets/images/favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="assets/images/favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="assets/images/favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon/favicon-16x16.png">
+<meta name="msapplication-TileImage" content="assets/images/favicon/ms-icon-144x144.png">
+
+  </head>
+  <body js-component="page">
+    <aside class="menu" js-component="menu">
+      <nav class="menu__nav nav">
+        
+        <ul class="nav__list nav__list--primary">
+          <li class="nav__list__item" js-menu-item>
+            <a class="nav__list__link nav__list__link--active" href="#" title="">
+              Home
+            </a>
+          </li>
+          <li class="nav__list__item" js-menu-item>
+            <a class="nav__list__link " href="#" title="">
+              About
+            </a>
+          </li>
+          <li class="nav__list__item" js-menu-item>
+            <a class="nav__list__link " href="#" title="">
+              Blog
+            </a>
+          </li>
+          <li class="nav__list__item" js-menu-item>
+            <a class="nav__list__link " href="#" title="">
+              Projects
+            </a>
+          </li>
+          <li class="nav__list__item" js-menu-item>
+            <a class="nav__list__link nav__list__link--highlight " href="#" title="">
+              Hire Me
+            </a>
+          </li>
+        </ul>
+        <ul class="nav__list nav__list--secondary">
+          <li class="nav__list__item" js-menu-item>
+            <a class="nav__list__link" href="mailto:mahsin.islam@yahoo.com">
+              email
+            </a>
+          </li>
+          <li class="nav__list__item" js-menu-item>
+            <a class="nav__list__link" href="https://twitter.com/islammahsin/" rel="nofollow" target="_blank">
+              twitter
+            </a>
+          </li>
+          <li class="nav__list__item" js-menu-item>
+            <a class="nav__list__link" href="http://stackoverflow.com/users/396300/timmy-omahony" rel="nofollow" target="_blank">
+              stackoverflow
+            </a>
+          </li>
+          <li class="nav__list__item" js-menu-item>
+            <a class="nav__list__link" href="https://github.com/mahsin-islam/" rel="nofollow" target="_blank">
+              github
+            </a>
+          </li>
+          <li class="nav__list__item" js-menu-item>
+            <a class="nav__list__link" href="https://www.codementor.io/mahsin" rel="nofollow" target="_blank">
+              codementor
+            </a>
+          </li>
+          <hr class="nav__list__divider" />
+         
+        </ul>
+      </nav>
+    </aside>
+    <div class="page">
+      <div class="menu-trigger menu-trigger--fixed" title="Toggle the sidebar navigation menu" js-component="menuTrigger">
+  <button class="menu-trigger__button" type="button" name="menu-trigger" js-menu-trigger>
+    <img class="menu-trigger__icon menu-trigger__icon--open" src="assets/images/menu-icon.svg" alt="Open Menu">
+    <img class="menu-trigger__icon menu-trigger__icon--close" src="assets/images/close.svg" alt="Close Menu">
+  </button>
+</div>
+
+
+<div class="centered-container">
+  <article class="home-page">
+    <section class="home-page__profile-card">
+      <div class="profile-card">
+  <div class="profile-card__portrait" title="I'm a web & software developer, primarily working with Django and Ember, living in Dublin, Ireland.
+">
+    <img src="assets/images/mahsin.png" alt="MD. Mahsin-Ul-Islam - Software Developer" />
+  </div>
+  <div class="profile-card__details">
+    <h1 class="profile-card__title">MD. Mahsin-Ul-Islam</h1>
+    <h2 class="profile-card__subtitle">Software Developer</h2>
+  </div>
+</div>
+
+    </section>
+    <section class="home-page__about">
+      <p>Hey, I&#39;m an independent web &amp; software developer from Dhaka, Bangladesh. I work primarily with <a href="http://oracle.com/">Java</a>, JavaScript, PHP and <a href="https://www.laravel.com/">Laravel</a>.</p>
+
+<p>Have a look through some of my <a href="#">projects</a> and <a href="#">blog posts</a>. Need help with an upcoming project or application? Learn a bit more <a href="#">about me</a> and then consider <a href="#">hiring me</a>.</p>
+
+    </section>
+    <ul class="icons-list">
+  <li class="icons-list__item">
+    <a href="mailto:mahsin.islam@yahoo.com" rel="nofollow" target="_blank" title="Send me an email">
+      <i class="fa fa-envelope" aria-hidden="true"></i>
+      Email
+    </a>
+  </li>
+  <li class="icons-list__item">
+    <a href="https://github.com/mahsin-islam/" rel="nofollow" target="_blank"  title="Look through my Github profile">
+      <i class="fa fa-github" aria-hidden="true"></i>
+      Github
+    </a>
+  </li>
+  <li class="icons-list__item">
+    <a href="https://twitter.com/islammahsin/" rel="nofollow" target="_blank" title="Tweet me!">
+      <i class="fa fa-twitter" aria-hidden="true"></i>
+      Twitter
+    </a>
+  </li>
+  <li class="icons-list__item">
+    <a href="https://www.linkedin.com/in/mahsinislam/" rel="nofollow" target="_blank"  title="See my LinkedIn profile">
+      <i class="fa fa-linkedin" aria-hidden="true"></i>
+      LinkedIn
+    </a>
+  </li>
+  <li class="icons-list__item">
+    <a href="https://stackoverflow.com/users/3699965/mahsin" rel="nofollow" target="_blank" title="See my StackOverflow profile">
+      <i class="fa fa-stack-overflow" aria-hidden="true"></i>
+      Stack Overflow
+    </a>
+  </li>
+  <li class="icons-list__item">
+    <a href="https://www.bitbucket.org/mahsin" rel="nofollow" target="_blank" title="Visit my Bitbucket profile">
+      <i class="fa fa-bitbucket" aria-hidden="true"></i>
+      BitBucket
+    </a>
+  </li>
+  <li class="icons-list__item">
+    <a href="https://codepen.io/mahsin/" rel="nofollow" target="_blank" title="Visit my Codepen profile">
+      <i class="fa fa-codepen" aria-hidden="true"></i>
+      Codepen
+    </a>
+  </li>
+  <li class="icons-list__item">
+  <a href="https://www.codementor.io/mahsin" rel="nofollow" target="_blank" title="Visit my Code Mentor profile">
+      <i class="fa fa-code" aria-hidden="true"></i>
+      Code Mentor
+    </a>
+  </li>
+  <li class="icons-list__item">
+    <a href="https://www.trello.com/mahsin" rel="nofollow" target="_blank" title="Find me on Trello">
+      <i class="fa fa-trello" aria-hidden="true"></i>
+      Trello
+    </a>
+  </li>
+  <li class="icons-list__item">
+    <a href="skype:md.mahsin.islam" rel="nofollow" target="_blank" title="Call me on Skype">
+      <i class="fa fa-skype" aria-hidden="true"></i>
+      Skype
+    </a>
+  </li>
+</ul>
+
+  </article>
+
+</div>
+
+      <div class="elevator" js-component="elevator" js-args="{ offset: 500 }">
+        <i class="elevator__icon fa fa-angle-up" aria-hidden="true"></i>
+      </div>
+    </div>
+  </body>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.min.js"></script>
+  <script src="assets/js/app.js"></script>
+
+  
+
+</html>
